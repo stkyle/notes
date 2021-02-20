b9153b811daac1c8d5a5646fb179c21563d9ddbb
stkyle <stkyle@users.noreply.github.com>
2021-02-20 13:14:55 -0500

    Update ethash_bnch_install.sh

--- ethash_bnch_install.sh
+++ ethash_bnch_install.sh
@@ -1,1 +1,6 @@
 /usr/bin
+tar -xzf lotsofdata.tar.gz <full path and filename from your list above>
+
+wget https://github.com/trexminer/T-Rex/releases/download/0.19.11/t-rex-0.19.11-linux-cuda11.1.tar.gz
+tar -xzf t-rex-0.19.11-linux-cuda11.1.tar.gz t-rex
+
